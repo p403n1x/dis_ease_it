1cd739600b30b952dc8dfc4741d700f4f467f0ae
olivier bouzon <oliv@hotmail.fr>
2020-02-17 17:48:43 +0100

    Associations & validations done

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :disease
   belongs_to :user
+  validates :start_date, presence: true
+  validates :duration, presence: true
 end
--- app/models/disease.rb
+++ app/models/disease.rb
@@ -1,2 +1,5 @@
 class Disease < ApplicationRecord
+  has_many :bookings
+  validates :name, presence: true
+  validates :description, presence: true
 end
