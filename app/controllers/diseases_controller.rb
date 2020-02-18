5b06059f177963d93113ae343be3e02296b5ea86
vanessa young <vybordeaux@gmail.com>
2020-02-18 14:38:11 +0100

    show controller and view

--- app/controllers/diseases_controller.rb
+++ app/controllers/diseases_controller.rb
@@ -1,8 +1,11 @@
 class DiseasesController < ApplicationController
+  skip_before_action :authenticate_user!
   def index
   end
 
   def show
+    @disease = Disease.find(params[:id])
+
   end
 
   def new
