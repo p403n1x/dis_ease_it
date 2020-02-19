ef397b229d10035e8b87da76d6051917b8aa57bd
vanessa young <vybordeaux@gmail.com>
2020-02-19 11:19:09 +0100

    added first last names and address to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   has_many :bookings
   has_many :diseases, through: :bookings
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name, uniqueness: { scope: :last_name }
+  validates :address, presence: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
