3f32709fe03e1d57f707324076da37eae6196ec0
vanessa young <vybordeaux@gmail.com>
2020-02-20 18:01:49 +0100

    seeds file modified to pull photos from cloudinary and not locally

--- app/controllers/diseases_controller.rb
+++ app/controllers/diseases_controller.rb
@@ -1,3 +1,5 @@
+require "open-uri"
+
 class DiseasesController < ApplicationController
   skip_before_action :authenticate_user!
 
@@ -17,13 +19,33 @@ class DiseasesController < ApplicationController
     @disease = Disease.new(disease_params)
     #@disease.image = "sample.jpg"
     @disease.user_id = current_user.id
-
+    @disease.image.attach(io: URI.open(params[:disease][:image]), filename: "photo.jpg", content_type: "image/jpg")
     if @disease.save
       redirect_to profil_path
     else
       render :new
     end
   end
+
+
+#def create
+    #@flat =Flat.new(flat_params)
+    #@flat.photo.attach(io: params[:flat][:photo], filename: “photo.jpg”, content_type: “image/jpg”)
+    #@flat.user = current_user
+    #if @flat.save
+      #redirect_to flat_path(@flat)
+    #else
+      #render :new
+    #end
+  #end
+
+
+
+
+
+
+
+
 
   def edit
     @disease = Disease.find(params[:id])
@@ -43,7 +65,7 @@ private
 
   def disease_params
 
-    params.require(:disease).permit(:name, :description, :user_id, :image)
+    params.require(:disease).permit(:name, :description, :user_id)
 
 
 
