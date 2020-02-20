b5098b183b2cee702d1967ccb2bd3e70f4d50a7b
anaelle claude <anaelleclaude@hotmail.fr>
2020-02-20 17:33:50 +0100

    delete disease et tout

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get '/profil', to: 'pages#profil'
+  get '/profil/index', to: 'pages#index'
+  delete '/profil/destroy', to: 'pages#destroy'
   resources :diseases  do
     resources :bookings, only: [:new, :create, :destroy]
   end
